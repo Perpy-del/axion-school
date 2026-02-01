098cbaac79abbcb6692be071767f955ca33a2834
Perpy-del <perpetualmeninwa@gmail.com>
2026-02-01 18:17:21 +0100

    server update

--- managers/http/UserServer.manager.js
+++ managers/http/UserServer.manager.js
@@ -1,39 +1,40 @@
-const http              = require('http');
-const express           = require('express');
-const cors              = require('cors');
-const app               = express();
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 module.exports = class UserServer {
-    constructor({config, managers}){
-        this.config        = config;
-        this.userApi       = managers.userApi;
-        this.managers      = managers;
-    }
-    
-    /** for injecting middlewares */
-    use(args){
-        app.use(args);
-    }
+  constructor({ config, managers }) {
+    this.config = config;
+    this.userApi = managers.userApi;
+    this.managers = managers;
+  }
 
-    /** server configs */
-    run(){
-        app.use(cors({origin: '*'}));
-        app.use(express.json());
-        app.use(express.urlencoded({ extended: true}));
-        app.use('/static', express.static('public'));
+  /** for injecting middlewares */
+  use(args) {
+    app.use(args);
+  }
 
-        /** an error handler */
-        app.use((err, req, res, next) => {
-            console.error(err.stack)
-            res.status(500).send('Something broke!')
-        });
-        
-        /** a single middleware to handle all */
-        app.all('/api/:moduleName/:fnName', this.userApi.mw);
+  /** server configs */
+  run() {
+    app.use(cors({ origin: "*" }));
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
+    app.use("/static", express.static("public"));
 
-        let server = http.createServer(app);
-        server.listen(this.config.PORT, () => {
-            console.log(`${(this.config.serviceName).toUpperCase()} is running on port: ${this.config.PORT}`);
-        });
-    }
-}
+    /** an error handler */
+    app.use((err, req, res, next) => {
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    });
+
+    /** a single middleware to handle all */
+    app.all("/api/:moduleName/:fnName", this.userApi.mw);
+
+    let server = http.createServer(app);
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, "0.0.0.0", () => {
+      console.log(`Server is online at port ${PORT}`);
+    });
+  }
+};
